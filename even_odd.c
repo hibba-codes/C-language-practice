ff5ddfa9ff118b9014f21c50772ccf4e0e3eae1d
hibba-codes <iqbalhibba10@gmail.com>
2025-12-27 00:31:14 -0800

    Added Even/Odd number program

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int num;
+printf("Enter Number");
+scanf("%d", num);
+if(num % 2==0){
+prinf{"Even.. %d\n", num);
+}
+else{
+printf("Odd..%d\n",num);
+}
+return 0;
